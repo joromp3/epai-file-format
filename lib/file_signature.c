66ebe58f94cfdf13c000295cc030f8a254268608
Jasen Borisov <tajjada@gmail.com>
2014-11-11 16:26:58 +0100

    Add function to verify file signature header.
    Add file_signature.c for code for the file signature section type.

---
+++ lib/file_signature.c
@@ -0,0 +1,53 @@
+/* See LICENSE file for copyright and license terms. */
+
+/*
+ * file_signature.c: implementation of functions related to the file
+ * signature section in the EPAI format.
+ */
+
+
+#include "common.h"
+#include "types.h"
+#include "file_signature.h"
+
+
+/* According to spec, all file signatures must be of exactly this length. */
+static const int file_signature_header_len = 11;
+
+
+/* Refuse to accept file signature headers with format version > this. */
+static int max_format_version = 0x00;
+
+
+extern epai_error_t epai_validate_file_signature(const char* buffer, int len) {
+	unsigned int endian_marker;
+
+	if (len != file_signature_header_len) {
+		return EPAI_ERROR_BAD_FILE_SIGNATURE;
+	}
+
+	if (*buffer != EPAI_SECTION_FILE_SIGNATURE) {
+		return EPAI_ERROR_SECTION_TYPE;
+	}
+
+	if (*++buffer != 'E' || *++buffer != 'P' ||
+	    *++buffer != 'A' || *++buffer != 'I' ||
+	    *++buffer != 0x49 || *++buffer != 0x0d ||
+	    *++buffer != 0x0a || *++buffer != 0x0a) {
+		return EPAI_ERROR_BAD_FILE_SIGNATURE;
+	}
+
+	if (*++buffer > max_format_version) {
+		return EPAI_ERROR_VERSION_UNSUPPORTED;
+	}
+
+	endian_marker = *++buffer;
+	endian_marker |= *++buffer << 8;
+
+	if (!(endian_marker == 0xAF00 || endian_marker == 0x00AF)) {
+		return EPAI_ERROR_BAD_FILE_SIGNATURE;
+	}
+
+	return EPAI_SUCCESS;
+}
+
---
+++ lib/file_signature.h
@@ -0,0 +1,19 @@
+/* See LICENSE file for copyright and license terms. */
+
+/* file_signature.h: header for file_signature.c */
+
+#ifndef LIBEPAI_FILE_SIGNATURE_H
+#define LIBEPAI_FILE_SIGNATURE_H
+
+
+/** \fn epai_error_t epai_validate_file_signature(const char* hdr, int len)
+    \brief Validate a file signature header in memory.
+    \param hdr Pointer to file signature header.
+    \param len Length of file signature header (must be 11).
+    \return EPAI_SUCCESS if valid, or the error if not.
+*/
+extern epai_error_t epai_validate_file_signature(const char*, int);
+
+
+#endif /* LIBEPAI_FILE_SIGNATURE_H */
+
