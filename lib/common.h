d10483006115d613150e28776afaee9a1a696404
Jasen Borisov <tajjada@gmail.com>
2014-11-15 08:02:02 +0100

    Improve header includes in common.h.

--- lib/common.h
+++ lib/common.h
@@ -9,19 +9,37 @@
 #  include <config.h>
 #endif
 
-#include <stdio.h>
-#include <sys/types.h>
+#if HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+#endif /* HAVE_SYS_TYPES_H */
 
-#if STDC_HEADERS
+#if HAVE_INTTYPES_H
+#  include <inttypes.h>
+#elif HAVE_STDINT_H
+#  include <stdint.h>
+#endif /* HAVE_INTTYPES_H */
+
+#if HAVE_STDLIB_H
 #  include <stdlib.h>
+#endif /* HAVE_STDLIB_H */
+
+#if HAVE_STRING_H
 #  include <string.h>
 #elif HAVE_STRINGS_H
 #  include <strings.h>
-#endif /* STDC_HEADERS */
+#endif /* HAVE_STRING_H */
+
+#if HAVE_MEMORY_H
+#  include <memory.h>
+#endif /* HAVE_MEMORY_H */
+
+#if HAVE_ENDIAN_H
+#  include <endian.h>
+#endif /* HAVE_ENDIAN_H */
 
 #if HAVE_UNISTD_H
 #  include <unistd.h>
-#endif
+#endif /* HAVE_MEMORY_H */
 
 
 #endif /* LIBEPAI_COMMON_H */
