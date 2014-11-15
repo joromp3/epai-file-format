64287704fc7c4850fe5608c021c6075d5f702740
Jasen Borisov <tajjada@gmail.com>
2014-11-15 09:29:51 +0100

    Add functions to generate new padding sections.

--- lib/padding.c
+++ lib/padding.c
@@ -37,3 +37,33 @@ extern epai_error_t epai_validate_padding(const char* buffer, uint32_t len) {
 
 	return EPAI_SUCCESS;
 }
+
+extern epai_error_t epai_fill_padding(char* buffer, uint32_t len) {
+	int i;
+
+	if (len < 5) {
+		return EPAI_ERROR_SECTION_LENGTH;
+	}
+
+	*buffer = EPAI_SECTION_PADDING;
+
+	*(uint32_t*)(buffer + 1) = len - 5;
+
+	for (i = 5; i < len; ++i) {
+		buffer[i] = 0;
+	}
+
+	return EPAI_SUCCESS;
+}
+
+extern char* epai_new_padding(uint32_t len) {
+	char* r = malloc(len);
+
+	if (r != NULL) {
+		if (epai_fill_padding(r, len)) {
+			free(r);
+		}
+	}
+
+	return r;
+}
--- lib/padding.h
+++ lib/padding.h
@@ -14,5 +14,20 @@
 */
 extern epai_error_t epai_validate_padding(const char*, uint32_t);
 
+/** \fn epai_error_t epai_fill_padding(char* buffer, uint32_t len)
+    \brief Generate a padding section in existing memory.
+    \param buffer Pointer to location to fill with padding.
+    \param len Length of padding section. Must be >=5.
+    \return EPAI_SUCCESS on success; EPAI_ERROR_SECTION_LENGTH if len invalid.
+*/
+extern epai_error_t epai_fill_padding(char*, uint32_t);
+
+/** \fn char* epai_new_padding(uint32_t len)
+    \brief Allocate new memory and generate a new padding section in it.
+    \param len The length of the new padding memory chunk. Must be >=5.
+    \return Pointer to the new padding section, or NULL on failure.
+*/
+extern char* epai_new_padding(uint32_t);
+
 
 #endif /* LIBEPAI_PADDING_H */
